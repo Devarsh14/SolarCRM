d1dbacfe1ef81edc5aa246159ce216b4c5b2d22e
Devarsh shukla <Devarsh14@gmail.com>
2019-03-16 15:08:58 +1000

    Add project files.

---
+++ SolarCRM/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SolarCRM.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
